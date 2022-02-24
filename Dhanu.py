9f163056e3158fb4d5120b89363fa99582d6191d
Geoffrey-Anto <geoffreyanto.ignatiuse2021@vitstudent.ac.in>
2022-02-25 02:41:19 +0530

    Final

--- Dhanu.py
+++ Dhanu.py
@@ -1,57 +1,68 @@
-data={}
+
+
+filename_input = "c_collaboration.in.txt"
+output_path = "C:/Users/Geoffrey/Documents/PROGRAMS/HASHCODE/output/"+filename_input
+
+
+data = {}
 pro = {}
-ans={}
+ans = {}
 checking = {}
+
+
 def prob(d):
     global checking
     checking = {}
-    sidelan=[]
+    sidelan = []
     for j in d:
         for k in data:
-            if j in data[k] and data[k][j]>=d[j]:
-                checking[k]=j
+            if j in data[k] and data[k][j] >= d[j]:
+                checking[k] = j
                 break
-        
         else:
             return False
     return True
-                    
+
+
 def update(a):
     for i in a:
         for j in data:
-            if i==j:
-                data[i][a[i]]=str(int(data[i][a[i]])+1)
+            if i == j:
+                data[i][a[i]] = str(int(data[i][a[i]])+1)
                 break
-    
-with open("C:/Users/Geoffrey/Documents/PROGRAMS/HASHCODE/a_an_example.in.txt", "r") as file:
-    file_list = file.readlines()
-    x=0;
-    a=list(map(int,file_list[x].split()))
-    x+=1;
-    for _ in range(a[0]):
-        i=file_list[x].split()
-        data[i[0]]={}
-        x+=1
-        for _ in range(int(i[1])):
-            d=file_list[x].split()
-            data[i[0]][d[0]]=d[1]
-            x+=1
-    for _ in range(a[1]):
-        a=file_list[x].split()
-        pro[a[0]]={}
-        x+=1
-        for i in range(int(a[-1])):
-            d=file_list[x].split()
-            pro[a[0]][d[0]]=d[1]
-            x+=1
 
-while len(ans)!=len(pro):
+
+with open("C:/Users/Geoffrey/Documents/PROGRAMS/HASHCODE/"+filename_input, "r") as file:
+    file_list = file.readlines()
+    x = 0
+    a = list(map(int, file_list[x].split()))
+    x += 1
+    for _ in range(a[0]):
+        i = file_list[x].split()
+        data[i[0]] = {}
+        x += 1
+        for _ in range(int(i[1])):
+            d = file_list[x].split()
+            data[i[0]][d[0]] = d[1]
+            x += 1
+    for _ in range(a[1]):
+        ab = file_list[x].split()
+        pro[ab[0]] = {}
+        x += 1
+        for i in range(int(ab[-1])):
+            d = file_list[x].split()
+            pro[ab[0]][d[0]] = d[1]
+            x += 1
+
+
+z = 0
+for _ in range(a[1]*5000):
     for i in pro:
         if prob(pro[i]):
-            ans[i]=checking
+            ans[i] = checking
             update(checking)
             break
-output_path = "C:/Users/Geoffrey/Documents/PROGRAMS/HASHCODE/output/a_an_example.in.txt"
+            z += 1
 
 with open(output_path, "w") as file:
     file.write(str(len(ans))+"\n")
--- MAIN.py
+++ MAIN.py
@@ -1,32 +1,64 @@
-def input():
-    with open("C:/Users/Geoffrey/Documents/PROGRAMS/HASHCODE/a_an_example.in.txt", "r") as file:
-        data = file.readlines()
-        x=0
-        c,p = data[x].split()
-        x+=1;
-        c=int(c);p=int(p);
-        contributors = []
-        for i in range(c):
-            name,skills = data[x].split()
-            x+=1;
-            contributors.append({name:[]})
-            for k in range(int(skills)):
-                skillname,skillpts = data[x].split()
-                contributors[i][name].append([skillname,int(skillpts)])
-                x+=1;
-        # print(contributors)
-        
-                
-                
-                
-                
-                
-            
-    
-    
-def main():
-    input()
-    return 0
+data={}
+pro = {}
+ans={}
+checking = {}
 
-if __name__=='__main__':
-    main()
+filename_input = "b_better_start_small.in.txt"
+
+def prob(d):
+    global checking
+    checking = {}
+    sidelan=[]
+    for j in d:
+        for k in data:
+            if j in data[k] and data[k][j]>=d[j]:
+                checking[k]=j
+                break
+        else:
+            return False
+    return True
+                    
+def update(a):
+    for i in a:
+        for j in data:
+            if i==j:
+                data[i][a[i]]=str(int(data[i][a[i]])+1)
+                break
+    
+with open("C:/Users/Geoffrey/Documents/PROGRAMS/HASHCODE/"+filename_input, "r") as file:
+    file_list = file.readlines()
+    x=0;
+    a=list(map(int,file_list[x].split()))
+    x+=1;
+    for _ in range(a[0]):
+        i=file_list[x].split()
+        data[i[0]]={}
+        x+=1
+        for _ in range(int(i[1])):
+            d=file_list[x].split()
+            data[i[0]][d[0]]=d[1]
+            x+=1
+    for _ in range(a[1]):
+        a=file_list[x].split()
+        pro[a[0]]={}
+        x+=1
+        for i in range(int(a[-1])):
+            d=file_list[x].split()
+            pro[a[0]][d[0]]=d[1]
+            x+=1
+
+while len(ans)!=len(pro):
+    for i in pro:
+        if prob(pro[i]):
+            ans[i]=checking
+            update(checking)
+            break
+output_path = "C:/Users/Geoffrey/Documents/PROGRAMS/HASHCODE/output/"+filename_input
+
+with open(output_path, "w") as file:
+    file.write(str(len(ans))+"\n")
+    for i in ans:
+        file.write(i+"\n")
+        for j in ans[i]:
+            file.write(j+" ")
+        file.writelines("\n")
